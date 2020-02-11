00c32a9585e72b712aca355d6ceab84468010762
Piyush <piyushtechsavy@gmail.com>
2020-02-11 10:46:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+def maxProfitDnC(prices):
+  print()
+
+def maxProfitLinear(prices):
+  size = len(prices)
+  dayIndxToBuy = 0
+  minIndex = 0
+  dayIndxToSell = 0
+  maxProf = prices[dayIndxToSell] - prices[dayIndxToBuy] 
+  minElement = prices[dayIndxToBuy] 
+      
+  for i in range( 1, size ): 
+    if (prices[i] - minElement > maxProf): 
+      maxProf = prices[i] - minElement 
+      dayIndxToSell = i
+      dayIndxToBuy = minIndex
+  
+    if (prices[i] < minElement): 
+      minElement = prices[i] 
+      minIndex = i
+  
+  
+  outputFile = open("outputPS8.txt", "w")
+  outputFile.write("Maximum Profit (Iterative solution): %d \n" %(maxProf))
+  outputFile.write("Day to buy: %d \n" %(dayIndxToBuy + 1))
+  outputFile.write("Day to sell: %d \n" %(dayIndxToSell + 1))
+
+  outputFile.close()
+  
+  
+
+  
+def readInputFile():
+  prices = []
+  inputFile = open("inputPS8.txt", "r")
+  for line in inputFile:
+    data = line.split("/")
+    price = int(data[1].strip())
+    prices.append(price)
+
+  inputFile.close()
+
+  return prices
+
+prices = readInputFile()
+maxProfitLinear(prices)
+print(prices)
